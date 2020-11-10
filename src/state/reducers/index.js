7f7465cd5f8b520b286d297b2d645c80126ce662
IT2 <it2@AWT.local>
2020-11-10 14:50:28 -0600

    added reducer and action fuction for products

--- src/state/reducers/index.js
+++ src/state/reducers/index.js
@@ -1,4 +1,8 @@
-// import all of your reducers into this file, and export them back out. 
-// This allows for the simplification of flow when importing reducers into your actions throughout your app.
+import { combineReducers } from 'redux';
+import productsReducer from './productsReducer';
 
+const reducers = combineReducers({
+  products: productsReducer,
+});
 
+export default reducers;
