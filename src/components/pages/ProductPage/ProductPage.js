1e5ec2addf6be90ed23d382066bff8224ba2dd25
Pedro Revilla <peterevilla@gmail.com>
2020-11-23 22:23:41 -0600

    created productpage component

---
+++ src/components/pages/ProductPage/ProductPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from '../../common/navBar/index';
+
+const ProductPage = () => {
+  return (
+    <div>
+      <NavBar />
+      ProductPage
+    </div>
+  );
+};
+
+export default ProductPage;
