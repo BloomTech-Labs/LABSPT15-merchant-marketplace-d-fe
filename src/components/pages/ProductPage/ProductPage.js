b445774f00d4d970d02e2696d6346f1474d6211e
Pedro Revilla Meza <pedrorevillameza@Pedros-MacBook-Air.local>
2020-11-30 22:50:41 -0600

    wip

--- src/components/pages/ProductPage/ProductPage.js
+++ src/components/pages/ProductPage/ProductPage.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import NavBar from '../../common/navBar/index';
+import './productPage.css';
+import ProductCarousel from './ProductCarousel';
 
 const ProductPage = () => {
   return (
     <div>
       <NavBar />
-      ProductPage
+      <div className="product-page">
+        <div className="product-container">
+          <div className="carousel">
+            <ProductCarousel />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
