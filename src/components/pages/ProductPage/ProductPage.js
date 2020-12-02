271a109f15b421c2886592b25a8d09bdfeaada79
IT2 <it2@AWT.local>
2020-12-02 13:17:43 -0600

    finished product page layout

--- src/components/pages/ProductPage/ProductPage.js
+++ src/components/pages/ProductPage/ProductPage.js
@@ -2,6 +2,8 @@ import React from 'react';
 import NavBar from '../../common/navBar/index';
 import './productPage.css';
 import ProductCarousel from './ProductCarousel';
+import { Rate, Avatar, Tag } from 'antd';
+import { GlobalOutlined } from '@ant-design/icons';
 
 const ProductPage = () => {
   return (
@@ -14,10 +16,34 @@ const ProductPage = () => {
           </div>
 
           <div className="item">
-            <h2>Item Name</h2>
-            <h2 className="price">$XX.XX</h2>
+            <div className="name-price">
+              <p>Item Name</p>
+              <p>$XX.XX</p>
+            </div>
+            <div className="rating">
+              <Rate />
+            </div>
+            <div className="store-name">
+              <Avatar size="small" icon={<GlobalOutlined />} />
+              <h3>Store Name</h3>
+            </div>
+            <p>Location</p>
+            <section>
+              <p>
+                (Short Description) Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit. Quisque a eleifend ante. Curabitur ac tincidunt
+                eros. Phasellus quis magna id massa iaculis congue. Integer
+                dapibus ullamcorper velit, in congue ex euismod at.
+              </p>
+            </section>
           </div>
         </div>
+        <section className="tags-container">
+          <Tag className="tags">Tag</Tag>
+          <Tag className="tags">Tag</Tag>
+          <Tag className="tags">Tag</Tag>
+          <Tag className="tags">Tag</Tag>
+        </section>
       </div>
     </div>
   );
