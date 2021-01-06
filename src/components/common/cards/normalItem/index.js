382456ec5edcf2fcd6f8c9fd70c96b7d6cc722e8
Andrei Lysenko <andrei.lysenko@outlook.com>
2021-01-05 20:48:44 -0800

    Temporary fix We added a fetch funtion to the item cards. That way they display the correct image with each car.

--- src/components/common/cards/normalItem/index.js
+++ src/components/common/cards/normalItem/index.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import { useOktaAuth } from '@okta/okta-react';
+import React, { useEffect, useState } from 'react';
+import { getDSData } from '../../../../api';
 import './itemCardStyles.css';
 
 function ItemCard({ name, description, price, image, count }) {
+  const [img, setImg] = useState('');
+  const { authState } = useOktaAuth();
+  const imgGet = id => {
+    getDSData(`http://localhost:8000/photo/${id}`, authState)
+      .then(res => setImg(res[0]['url']))
+      .catch(err => {
+        console.log('Img get fail.');
+      });
+  };
+  useEffect(() => {
+    imgGet(image);
+  }, []);
+
   return (
     <div className="cardContainer">
-      <img src={image} className="cardImage" />
+      <img src={img} className="cardImage" />
       <div className="cardDesc">
         <h2 className="descText">{name}</h2>
         <p className="descText">{description}</p>
--- src/components/sellerPages/inventory/newItem/photos.js
+++ src/components/sellerPages/inventory/newItem/photos.js
@@ -1,70 +1,81 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
 import FormButton from '../../../common/FormButton/FormButton';
 import { Upload, message } from 'antd';
 import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
+import uploadcare from 'uploadcare-widget';
+import { addItemImage } from '../../../../state/actions';
+import { useOktaAuth } from '@okta/okta-react';
+
 function AddPhotos({ setProgress, slider, setItem }) {
-  const [data, setData] = useState({
-    loading: false,
-    image: null,
-  });
-  const getBase64 = (img, callback) => {
-    const reader = new FileReader();
-    reader.addEventListener('load', () => callback(reader.result));
-    reader.readAsDataURL(img);
-  };
+  const { authState } = useOktaAuth();
+  function openUploadDialog(e) {
+    let dialog = uploadcare.openDialog(null, {
+      publicKey: '7f074009b333b2d5be63',
+      imagesOnly: true,
+    });
+    dialog.done(function(file, fileGroup, list) {
+      file.promise().done(function(fileInfo) {
+        console.log('fileinfo: ', fileInfo);
+        addItemImage(authState, 15, fileInfo.originalUrl);
+      });
+    });
+  }
 
-  const beforeUpload = file => {
-    const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
-    if (!isJpgOrPng) {
-      message.error('You can only upload JPG/PNG file!');
-    }
-    const isLt2M = file.size / 1024 / 1024 < 2;
-    if (!isLt2M) {
-      message.error('Image must smaller than 2MB!');
-    }
-    return isJpgOrPng && isLt2M;
-  };
+  // const [data, setData] = useState({
+  //   loading: false,
+  //   image: null,
+  // });
 
-  const handleChange = info => {
-    if (info.file.status === 'uploading') {
-      setData({ loading: true });
-      return;
-    }
-    if (info.file.status === 'done') {
-      // Get this url from response in real world.
-      getBase64(info.file.originFileObj, imageUrl =>
-        setData({
-          imageUrl: imageUrl,
-          loading: false,
-        })
-      );
-    }
-  };
-  const uploadButton = (
-    <div>
-      {data.loading ? <LoadingOutlined /> : <PlusOutlined />}
-      <div style={{ marginTop: 8 }}>Upload</div>
-    </div>
-  );
+  // const getBase64 = (img, callback) => {
+  //   const reader = new FileReader();
+  //   reader.addEventListener('load', () => callback(reader.result));
+  //   reader.readAsDataURL(img);
+  // };
+
+  // const beforeUpload = file => {
+  //   const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+  //   if (!isJpgOrPng) {
+  //     message.error('You can only upload JPG/PNG file!');
+  //   }
+  //   const isLt2M = file.size / 1024 / 1024 < 2;
+  //   if (!isLt2M) {
+  //     message.error('Image must smaller than 2MB!');
+  //   }
+  //   return isJpgOrPng && isLt2M;
+  // };
+
+  // const handleChange = info => {
+  //   if (info.file.status === 'uploading') {
+  //     setData({ loading: true });
+  //     return;
+  //   }
+  //   if (info.file.status === 'done') {
+  //     // Get this url from response in real world.
+  //     getBase64(info.file.originFileObj, imageUrl =>
+  //       setData({
+  //         imageUrl: imageUrl,
+  //         loading: false,
+  //       })
+  //     );
+  //   }
+  // };
+  // const uploadButton = (
+  //   <div>
+  //     {data.loading ? <LoadingOutlined /> : <PlusOutlined />}
+  //     <div style={{ marginTop: 8 }}>Upload</div>
+  //   </div>
+  // );
 
   return (
     <div className="contents">
       <h1>Add photos of the item</h1>
-      <Upload
-        name="avatar"
-        listType="picture-card"
-        className="avatar-uploader"
-        showUploadList={false}
-        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-        beforeUpload={beforeUpload}
-        onChange={handleChange}
+      <div
+        onClick={openUploadDialog}
+        className="ant-upload ant-upload-select ant-upload-select-picture-card"
       >
-        {data.image ? (
-          <img src={data.image} alt="avatar" style={{ width: '100%' }} />
-        ) : (
-          uploadButton
-        )}
-      </Upload>
+        +
+      </div>
 
       <FormButton
         setProgress={setProgress}
@@ -76,4 +87,4 @@ function AddPhotos({ setProgress, slider, setItem }) {
   );
 }
 
-export default AddPhotos;
+export default connect(null, { addItemImage })(AddPhotos);
