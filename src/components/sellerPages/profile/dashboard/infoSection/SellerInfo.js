ba3ece8cd182e126e5a7feadb2e9b8cb655238e3
aland1013 <adam.land@gmail.com>
2021-02-08 20:15:51 -0500

    added edit button to seller info section of seller dashboard

--- src/components/sellerPages/profile/dashboard/infoSection/SellerInfo.js
+++ src/components/sellerPages/profile/dashboard/infoSection/SellerInfo.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import { useOktaAuth } from '@okta/okta-react/src/OktaContext';
 import { connect } from 'react-redux';
+import { Button } from 'antd';
+import { EditOutlined } from '@ant-design/icons';
 import { fetchSellerProfile } from '../../../../../state/actions';
 
 const SellerInfo = ({ sellerInfo, fetchSellerProfile }) => {
@@ -13,11 +15,18 @@ const SellerInfo = ({ sellerInfo, fetchSellerProfile }) => {
   return (
     <>
       <h2>Seller Info</h2>
-      <p>{sellerInfo.seller_name}</p>
+      <h4>{sellerInfo.seller_name}</h4>
       <p>{sellerInfo.description}</p>
+      <br />
       <p>{sellerInfo.physical_address}</p>
+      <br />
       <p>{sellerInfo.phone_number}</p>
+      <br />
       <p>{sellerInfo.email_address}</p>
+      <br />
+      <Button icon={<EditOutlined />} size="small">
+        edit
+      </Button>
     </>
   );
 };
