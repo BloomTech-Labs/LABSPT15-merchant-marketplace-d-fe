5b83b8c659a5c995a31fbe2d8f075bb2ba65975e
aland1013 <adam.land@gmail.com>
2021-02-09 22:26:35 -0500

    added validation to the seller profile form

--- src/components/sellerPages/profile/dashboard/infoSection/EditProfileForm.js
+++ src/components/sellerPages/profile/dashboard/infoSection/EditProfileForm.js
@@ -4,6 +4,15 @@ import { Modal, Input, Form } from 'antd';
 const EditProfileForm = ({ visible, fields, onSubmit, onCancel }) => {
   const [form] = Form.useForm();
 
+  const validatePhone = (rule, value, callback) => {
+    const regex = new RegExp(/\([0-9]{3}\)\s[0-9]{3}-[0-9]{4}/);
+    if (!regex.test(value)) {
+      callback('Phone number must be (xxx) xxx-xxxx');
+    } else {
+      callback();
+    }
+  };
+
   return (
     <Modal
       visible={visible}
@@ -32,19 +41,71 @@ const EditProfileForm = ({ visible, fields, onSubmit, onCancel }) => {
           modifier: 'public',
         }}
       >
-        <Form.Item name="seller_name" label="Name">
+        <Form.Item
+          name="seller_name"
+          label="Name"
+          rules={[
+            {
+              required: true,
+              message: 'Please enter a name',
+            },
+          ]}
+        >
           <Input />
         </Form.Item>
-        <Form.Item name="description" label="Description">
+        <Form.Item
+          name="description"
+          label="Description"
+          rules={[
+            {
+              required: true,
+              message: 'Please enter a description',
+            },
+          ]}
+        >
           <Input.TextArea />
         </Form.Item>
-        <Form.Item name="physical_address" label="Address">
+        <Form.Item
+          name="physical_address"
+          label="Address"
+          rules={[
+            {
+              required: true,
+              message: 'Please enter an address',
+            },
+          ]}
+        >
           <Input />
         </Form.Item>
-        <Form.Item name="phone_number" label="Phone">
+        <Form.Item
+          name="phone_number"
+          label="Phone"
+          rules={[
+            {
+              required: true,
+              message: 'Please enter a phone number',
+            },
+            {
+              validator: validatePhone,
+            },
+          ]}
+        >
           <Input />
         </Form.Item>
-        <Form.Item name="email_address" label="Email">
+        <Form.Item
+          name="email_address"
+          label="Email"
+          rules={[
+            {
+              type: 'email',
+              message: 'Invalid email address',
+            },
+            {
+              required: true,
+              message: 'Please input your email address',
+            },
+          ]}
+        >
           <Input />
         </Form.Item>
       </Form>
