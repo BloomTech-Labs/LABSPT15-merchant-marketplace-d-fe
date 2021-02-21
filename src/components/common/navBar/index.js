04562a9d78b71892fdd386bbc804d540ed1df744
BkAngel201 <33973377+BkAngel201@users.noreply.github.com>
2021-02-20 23:39:14 -0500

    this files had to be in another branch

--- src/components/common/navBar/index.js
+++ src/components/common/navBar/index.js
@@ -6,7 +6,6 @@ import './navStyles.css';
 import SearchBar from '../searchbar';
 import { useOktaAuth } from '@okta/okta-react';
 import logo from '../inventory-logo.png';
-import OrderCart from '../orderCart';
 
 function NavBar({ searchVisible, data, setData }) {
   const { authState, authService } = useOktaAuth();
@@ -22,7 +21,6 @@ function NavBar({ searchVisible, data, setData }) {
         <Link>Orders</Link>
         <Link>Payment</Link>
         <Link>Messages</Link>
-        <OrderCart />
       </div>
       <SearchBar searchVisible={searchVisible} setData={setData} data={data} />
     </div>
